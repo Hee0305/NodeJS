76d24bb3a834a7375cd05c2663c9eed5faa10ccd
Hee0305 <86734285+Hee0305@users.noreply.github.com>
2021-07-16 19:50:07 +0900

    Add files via upload

---
+++ 7_16/server1-3.js
@@ -0,0 +1,19 @@
+const http = require('https');
+const fs = require('fs');
+const { Http2ServerRequest } = require('http2');
+
+Http2ServerRequest.createServer({
+    cert: fs.readFileSync('도메인 인증서 경로'),
+    key: fs.readFileSync('도메인 비밀키 경로'),
+    ca: [
+        fs.readFileSync('상위 인증서 경로'),
+        fs.readFileSync('상위 인증서 경로'),
+    ],
+}, (req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8' });
+    res.write('<h1>Hello Node!</h2>');
+    res.end('<p>Hello Server!</p>');
+})
+.listen(443, () => {
+    console.log('443번 포트에서 서버 대기 중입니다.');
+});
